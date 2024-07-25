bfd1e6c3e21120f1a4790249695fb96955d0c739
trashrama <canaisdosantiago@gmail.com>
2024-07-25 11:04:44 -0300

    criei o projeto java e o DAL

---
+++ slow/src/com/slow/dal/Conexao.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.slow.dal;
+import java.sql.*;
+
+/**
+ *
+ * @author sant
+ */
+public class Conexao {
+    public static Connection conector(){
+        java.sql.Connection connect = null;
+        String url = "jdbc:mysql://localhost:3306/slowdb";
+        String user = "root";
+        String password = "99586090";
+        
+        try {
+            connect = DriverManager.getConnection(url, user, password);
+            return connect;
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+        }
+    }
+}
