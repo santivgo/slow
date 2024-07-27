030289bbbbc6a63abbff7787eb80bb2c4f40d1f1
trashrama <canaisdosantiago@gmail.com>
2024-07-26 21:04:02 -0300

    consegui fazer um grid do jeito que eu queria. agora é só substituir pelas imagens que estao no banco de dados, redimensionar e ajustar as larguras. se eu morrer hj morro feliz.

--- slow/src/com/slow/telas/TelaInicial.java
+++ slow/src/com/slow/telas/TelaInicial.java
@@ -3,7 +3,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 package com.slow.telas;
-
+import java.sql.*;
+import com.slow.dal.Conexao;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
 /**
  *
  * @author aluno
@@ -13,8 +16,60 @@ public class TelaInicial extends javax.swing.JFrame {
     /**
      * Creates new form telaInicial
      */
+    
+    Connection conexao = null;
+    PreparedStatement pst = null;
+    ResultSet rs = null; 
+    
     public TelaInicial() {
         initComponents();
+        conexao = Conexao.conector();
+        pegarTamanho();
+        popular();
+        
+    }
+    
+    private int pegarTamanho(){
+        String sql = "select count(id_midia) from midia";
+        
+        try{
+            pst = conexao.prepareStatement(sql);
+            pst.executeQuery();
+            
+            if (rs.next()){
+                int tamanho = (int) Math.round(((double)rs.getInt(1) / 2)+0.5d);
+                return tamanho;
+            }else{
+                return 0;
+            }
+            
+            
+        }catch (Exception e){
+            return 0;
+        }
+        
+    }
+    
+    private void popular(){
+        String sql = "SELECT * from midia";
+        
+        try{
+            pst = conexao.prepareStatement(sql);
+            rs = pst.executeQuery();
+            
+            while(rs.next()){
+                int i = 0;
+                JButton button = new JButton("Button " + Integer.toString(0));
+                System.out.println(rs.getBlob(5));
+                jPanel1.add(button);
+            }
+            
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(rootPane, e);
+        }
+        
+        
+        
     }
 
     /**
@@ -26,11 +81,16 @@ public class TelaInicial extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         jMenu2 = new javax.swing.JMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new java.awt.GridLayout(1, 0));
+
+        jPanel1.setLayout(new java.awt.GridLayout(pegarTamanho(), 4));
+        getContentPane().add(jPanel1);
 
         jMenu1.setText("File");
         jMenuBar1.add(jMenu1);
@@ -39,17 +99,6 @@ public class TelaInicial extends javax.swing.JFrame {
         jMenuBar1.add(jMenu2);
 
         setJMenuBar(jMenuBar1);
-
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 860, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 405, Short.MAX_VALUE)
-        );
 
         pack();
         setLocationRelativeTo(null);
@@ -95,5 +144,6 @@ public class TelaInicial extends javax.swing.JFrame {
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
