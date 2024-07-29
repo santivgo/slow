8a8f612cba9efbaf732e33fc8f0ae51fabafcbd8
trashrama <canaisdosantiago@gmail.com>
2024-07-28 22:24:07 -0300

    adicionado mais itens e agora o usuario pode alugar as midias e reverter se ele nao quiser mais alugar

--- slow/src/com/slow/telas/TelaInicial.java
+++ slow/src/com/slow/telas/TelaInicial.java
@@ -4,9 +4,21 @@
  */
 package com.slow.telas;
 import java.sql.*;
+import java.awt.Image;
+import java.awt.Toolkit;
 import com.slow.dal.Conexao;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.SwingConstants;
 /**
  *
  * @author aluno
@@ -20,14 +32,16 @@ public class TelaInicial extends javax.swing.JFrame {
     Connection conexao = null;
     PreparedStatement pst = null;
     ResultSet rs = null; 
+    public static String id_midia = null;
     
     public TelaInicial() {
         initComponents();
         conexao = Conexao.conector();
-        pegarTamanho();
-        popular();
-        
+        popular();        
+        jScrollPane2.setHorizontalScrollBar(null);
     }
+    
+    
     
     private int pegarTamanho(){
         String sql = "select count(id_midia) from midia";
@@ -51,25 +65,65 @@ public class TelaInicial extends javax.swing.JFrame {
     }
     
     private void popular(){
-        String sql = "SELECT * from midia";
+        String sql = "SELECT id_midia, nome, img from midia";
         
         try{
             pst = conexao.prepareStatement(sql);
             rs = pst.executeQuery();
             
             while(rs.next()){
-                int i = 0;
-                JButton button = new JButton("Button " + Integer.toString(0));
-                System.out.println(rs.getBlob(5));
+
+                byte[] imageBytes = rs.getBytes(3);
+                Image image=getToolkit().createImage(imageBytes);
+                
+                Image img = image.getScaledInstance(100,150,Image.SCALE_SMOOTH);
+                
+                JLabel label = new JLabel(rs.getString(2), new ImageIcon(img), SwingConstants.LEADING);
+                label.setFont(new Font("Ubuntu", Font.BOLD, 14));
+                label.setHorizontalTextPosition(JLabel.CENTER);
+                label.setVerticalTextPosition(JLabel.BOTTOM);
+                label.setHorizontalAlignment(SwingConstants.CENTER);
+                label.setVerticalAlignment(SwingConstants.CENTER);
+                label.setBorder(BorderFactory.createEmptyBorder(1,1,1,1));
+
+                JButton button = new JButton();
+                button.setBackground(new Color(255, 255, 255));
+                button.setLayout(new GridLayout(0, 1));
+                button.add(label);
+                button.setPreferredSize(new Dimension(140, 150));
+                button.setMaximumSize(new Dimension(140, 150));
+                button.setMinimumSize(new Dimension(140, 150));
+                
+                // é oq deixa feio
+                //button.setContentAreaFilled(false);
+                
+                button.setActionCommand(rs.getString(1)); // guarda o id como identificador
+                
+                button.addMouseListener(new MouseAdapter(){
+                    @Override
+                    public void mouseClicked(MouseEvent e){
+                        JButton botaoClicked = (JButton) e.getSource();
+                        id_midia = botaoClicked.getActionCommand();
+                        
+                        TelaInfoMidias tim = new TelaInfoMidias();
+                        tim.setVisible(true);
+                        
+                    }
+                });
+
+                
+
+
+                jPanel1.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
                 jPanel1.add(button);
+        
             }
             
         }catch (Exception e){
             JOptionPane.showMessageDialog(rootPane, e);
         }
         
-        
-        
+       
     }
 
     /**
@@ -81,16 +135,39 @@ public class TelaInicial extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jScrollPane2 = new javax.swing.JScrollPane();
         jPanel1 = new javax.swing.JPanel();
+        jPanel2 = new javax.swing.JPanel();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         jMenu2 = new javax.swing.JMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        getContentPane().setLayout(new java.awt.GridLayout(1, 0));
+        setBackground(new java.awt.Color(255, 255, 255));
+        setMinimumSize(new java.awt.Dimension(610, 430));
+        setResizable(false);
 
-        jPanel1.setLayout(new java.awt.GridLayout(pegarTamanho(), 4));
-        getContentPane().add(jPanel1);
+        jScrollPane2.setPreferredSize(new java.awt.Dimension(750, 430));
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel1.setMinimumSize(new java.awt.Dimension(750, 430));
+        jPanel1.setPreferredSize(new java.awt.Dimension(730, 430));
+        jPanel1.setLayout(new java.awt.GridLayout(pegarTamanho(), 4, 10, 10));
+        jScrollPane2.setViewportView(jPanel1);
+
+        jPanel2.setMinimumSize(new java.awt.Dimension(204, 430));
+        jPanel2.setPreferredSize(new java.awt.Dimension(204, 430));
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 430, Short.MAX_VALUE)
+        );
 
         jMenu1.setText("File");
         jMenuBar1.add(jMenu1);
@@ -99,6 +176,26 @@ public class TelaInicial extends javax.swing.JFrame {
         jMenuBar1.add(jMenu2);
 
         setJMenuBar(jMenuBar1);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 750, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, 198, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(12, 12, 12))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 430, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(0, 0, Short.MAX_VALUE))
+        );
 
         pack();
         setLocationRelativeTo(null);
@@ -145,5 +242,7 @@ public class TelaInicial extends javax.swing.JFrame {
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JScrollPane jScrollPane2;
     // End of variables declaration//GEN-END:variables
 }
